32d8820466ca0c32405ae6cff2ebdece35487408
Letancio Marinho <61944081+Letancio@users.noreply.github.com>
2023-09-22 16:15:47 -0300

    Update test.php
    version final

--- test.php
+++ test.php
@@ -1,43 +1,58 @@
 <?php
 
-// Crie ou conecte-se a um banco de dados SQLite em memória
-$memory_db = new SQLite3(':memory:');
+function test_sql_php() {
+    // Configurações do banco de dados MySQL
+    $hostname = "localhost";  // Nome do servidor
+    $username = "root"; // Nome de usuário do MySQL
+    $password = "";   // Senha do MySQL
+    $database = "php_test"; // Nome do banco de dados
 
-// Verifique se a conexão foi estabelecida corretamente
-if (!$memory_db) {
-    die("Falha na criação do banco de dados em memória.");
-}
+    // Crie uma conexão com o banco de dados MySQL
+    $mysqli = new mysqli($hostname, $username, $password, $database);
 
-// Abra o arquivo SQL
-$sql_file = "arquive.sql"; // Substitua pelo nome do seu arquivo SQL
-$fp = fopen($sql_file, "r");
+    // Verifique a conexão
+    if ($mysqli->connect_error) {
+        die("Falha na conexão: " . $mysqli->connect_error);
+    }
 
-// Inicialize com sucesso
-$success = true;
+    // Abra o arquivo SQL
+    $sql_file = "131_receita.sql"; // Substitua pelo nome do seu arquivo SQL
+    $fp = fopen($sql_file, "r");
 
-// Execute as consultas SQL no arquivo
-while (!feof($fp)) {
-    $sql = fgets($fp);
-    if (trim($sql) !== '') { // Verifique se a linha não está vazia
-        $result = $memory_db->exec($sql); // Execute a instrução SQL no banco de dados em memória
-        if ($result === false) {
-            $success = false;
-            break;
+    // Inicialize com sucesso
+    $success = true;
+
+    // Execute as consultas SQL no arquivo
+    $query = "";
+    while (!feof($fp)) {
+        $line = fgets($fp);
+        if (trim($line) !== '') { // Verifique se a linha não está vazia
+            $query .= $line;
+            if (substr(trim($line), -1) == ';') { // Se a linha terminar com ponto e vírgula, execute a consulta
+                if ($mysqli->query($query) !== TRUE) { // Execute a instrução SQL no banco de dados MySQL
+                    $success = false;
+                    break;
+                }
+                $query = ""; // Limpe a consulta para a próxima instrução
+            }
         }
     }
+
+    // Feche o arquivo SQL
+    fclose($fp);
+
+    // Verifique o resultado do teste
+    if ($success) {
+        echo "O arquivo SQL passou no teste de integridade! Nome do arquivo: " . $sql_file . "<br><br><br>";
+    } else {
+        echo "O arquivo SQL falhou no teste de integridade: " . $sql_file . "<br>Erro: " . $mysqli->error . "<br><br><br>";
+    }
+
+    // Feche a conexão com o banco de dados MySQL
+    $mysqli->close();
 }
 
-// Feche o arquivo SQL
-fclose($fp);
-
-// Verifique o resultado do teste
-if ($success) {
-    echo "O arquivo SQL passou no teste de integridade!";
-} else {
-    echo "O arquivo SQL falhou no teste de integridade.";
-}
-
-// Feche a conexão com o banco de dados em memória
-$memory_db->close();
+// Chame a função para testar o SQL
+test_sql_php();
 
 ?>
