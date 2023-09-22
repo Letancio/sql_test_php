0b9004d96a5ffa66ed0f6b275af658b8420aad11
Letancio Marinho <61944081+Letancio@users.noreply.github.com>
2023-09-22 15:50:41 -0300

    Update test.php
    upload new version

--- test.php
+++ test.php
@@ -1,55 +1,43 @@
 <?php
 
-class sql_test
-{
-    public function sql_test_integration(): bool
-    {
-        try {
-            // Conecte-se ao banco de dados SQLite em memória
-            $conn = new MYSQL(':memory:');
-            
-            // Testing Virtual DataBase Memory Temp
-            $conn->exec("PRAGMA foreign_keys = ON;");
-            
-            // for Open Sql File
-            $fp = fopen("arquive.sql", "r");
-            
-            // Starting for Testing
-            $success = true; // Inicialize com true
-            while (!feof($fp)) {
-                $sql = fgets($fp);
-                if (trim($sql) !== '') { // Verifique se a linha não está vazia
-                    $result = $conn->exec($sql); // Use exec() para comandos SQL que não retornam resultados
-                    if ($result === false) {
-                        $success = false;
-                        break;
-                    }
-                }
-            }
-            
-            // Closed Arquive Sql
-            fclose($fp);
-            
-            // Feche a conexão com o banco de dados SQLite em memória
-            $conn->close();
-            
-            return $success;
-        } catch (Exception $e) {
-            echo "O teste falhou: " . $e->getMessage();
-            return false;
+// Crie ou conecte-se a um banco de dados SQLite em memória
+$memory_db = new SQLite3(':memory:');
+
+// Verifique se a conexão foi estabelecida corretamente
+if (!$memory_db) {
+    die("Falha na criação do banco de dados em memória.");
+}
+
+// Abra o arquivo SQL
+$sql_file = "arquive.sql"; // Substitua pelo nome do seu arquivo SQL
+$fp = fopen($sql_file, "r");
+
+// Inicialize com sucesso
+$success = true;
+
+// Execute as consultas SQL no arquivo
+while (!feof($fp)) {
+    $sql = fgets($fp);
+    if (trim($sql) !== '') { // Verifique se a linha não está vazia
+        $result = $memory_db->exec($sql); // Execute a instrução SQL no banco de dados em memória
+        if ($result === false) {
+            $success = false;
+            break;
         }
     }
 }
 
-// Crie uma instância da classe sql_test
-$sqlTest = new sql_test();
+// Feche o arquivo SQL
+fclose($fp);
 
-// Chame o método sql_test_integration e trate o resultado
-$success = $sqlTest->sql_test_integration();
-
+// Verifique o resultado do teste
 if ($success) {
-    echo "O teste foi bem-sucedido!";
+    echo "O arquivo SQL passou no teste de integridade!";
 } else {
-    echo "O teste falhou.";
+    echo "O arquivo SQL falhou no teste de integridade.";
 }
+
+// Feche a conexão com o banco de dados em memória
+$memory_db->close();
+
 ?>
